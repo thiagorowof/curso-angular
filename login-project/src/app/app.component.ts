0eb691455d9c02099a4f7f8c9e4c4b6080c3dca3
Rafael Melo <contato@rafaelmelo.com.br>
2020-02-17 17:33:05 -0300

    Login

--- login-project/src/app/app.component.ts
+++ login-project/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login-project';
+
+  username:string;
+  password: string;
+ 
+  constructor() { }
+
+  logUser()
+  {
+    if (this.username == "Admin" && this.password == "admin123")
+    {
+      console.log("Welcome...." + this.username )
+    } else {
+      alert("Usuário ou Senha incorretas....")
+    }
+  }
 }
--- login-project/src/app/login/login.component.ts
+++ login-project/src/app/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username:string;
+  password: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  logInUser()
+  {
+    if (this.username == "Admin" && this.password == "admin123")
+    {
+      console.log("Welcome....")
+    }
+  }
 }
