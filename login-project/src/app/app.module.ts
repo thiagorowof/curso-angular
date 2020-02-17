cea01c384302728774fe5c65b0fade9ef9ad308d
Rafael Melo <contato@rafaelmelo.com.br>
2020-02-17 17:11:56 -0300

    Login Component

--- login-project/src/app/app.module.ts
+++ login-project/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
