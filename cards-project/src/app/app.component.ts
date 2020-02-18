e33499485d5aff9b4da0a4f54480b43630e768d1
Rafael Melo <contato@rafaelmelo.com.br>
2020-02-18 18:21:37 -0300

    Cards

---
+++ cards-project/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  posts =[
+    { 
+      title: 'Pereira DEV',
+      imageUrl: 'assets/pereira.jpeg',
+      username: '@pereirarodriguesfael',
+      content: 'Nome de usuário muito grande! tem que padronizar :)'
+    },
+    { 
+      title: 'Pereira DEV',
+      imageUrl: 'assets/pereira.jpeg',
+      username: '@pereirarodriguesfael',
+      content: 'Nome de usuário muito grande! tem que padronizar :)'
+    },
+    { 
+      title: 'Pereira DEV',
+      imageUrl: 'assets/pereira.jpeg',
+      username: '@pereirarodriguesfael',
+      content: 'Nome de usuário muito grande! tem que padronizar :)'
+    },
+    { 
+      title: 'Pereira DEV',
+      imageUrl: 'assets/pereira.jpeg',
+      username: '@pereirarodriguesfael',
+      content: 'Nome de usuário muito grande! tem que padronizar :)'
+    },
+    { 
+      title: 'Pereira DEV',
+      imageUrl: 'assets/pereira.jpeg',
+      username: '@pereirarodriguesfael',
+      content: 'Nome de usuário muito grande! tem que padronizar :)'
+    }
+  ]
+}
