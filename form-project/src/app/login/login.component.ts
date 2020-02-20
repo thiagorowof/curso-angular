31f48029658f9c0a34237ee1c7381b5de6a6a611
Rafael Melo <contato@rafaelmelo.com.br>
2020-02-20 17:17:39 -0300

    Form Project

--- form-project/src/app/login/login.component.ts
+++ form-project/src/app/login/login.component.ts
@@ -8,31 +8,24 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   
-  username:string;
-  password: string;
+  username:string = '';
+  password: string = '';
   msg: string;
   status: string;
 
   constructor(public router: Router) {}
 
   ngOnInit(): void {
+
   }
 
   logInUser(){
     if (this.username.toLowerCase() == "admin" && this.password.toLowerCase() == "admin")
     {
-      // console.log("Welcome...." + this.username )
-      // alert("Welcome...." + this.username )
-      this.status = "success"
-      this.msg = "Usuário Validado! Redirecionando..."
-      // window.location.href = '/home';
-      // this.router.navigateByUrl('/home');
       this.router.navigateByUrl('/cliente');
     } else {
-      // alert("Usuário ou Senha incorretas....")
       this.status = "danger"
       this.msg = "Nome de usuário ou senha incorretos"
-      // console.log("Welcome...." + this.username )
     }
   }
   
