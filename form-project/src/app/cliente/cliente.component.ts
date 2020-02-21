1bd5e57ec910232f6de2c132be202dd6f3fbf666
Rafael Melo <contato@rafaelmelo.com.br>
2020-02-21 14:11:34 -0300

    Alguma coisa :)

--- form-project/src/app/cliente/cliente.component.ts
+++ form-project/src/app/cliente/cliente.component.ts
@@ -12,6 +12,7 @@ import { Router } from '@angular/router';
   export class ClienteComponent {
 
   username = "Falta importar de login"
+  
 
   constructor() { }
   // constructor(public activatedRoute: Router) {
